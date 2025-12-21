160ad31f09bf61631637932664b3c062e5f8a5a0
kdwong <kw342@cornell.edu>
2025-12-21 21:34:42 +0800

    theorems on addition of surreals added - have fun!

--- Surreal/Arithmetic.lean
+++ Surreal/Arithmetic.lean
@@ -17,3 +17,64 @@ def Game.add : Game → Game → Game
     · sorry
     · sorry
     · sorry
+
+theorem Game.add_zero (a : Game) : Game.add a zero = a := by
+  sorry
+
+theorem Game.zero_add (a : Game) : Game.add zero a = a := by
+  sorry
+
+
+-- These two lemmas must be proved hand-in-hand by induction on
+-- birthday(a) + birthday(b) + birthday(a') + birthday(b').
+theorem Game.AddThm1 (a b a' b' : Game) :
+  (a.le a) ∧ (b.le b) → (Game.add a b).le (Game.add a' b') := by
+  sorry
+
+theorem Game.AddThm2  (a b a' b' : Game) :
+  (Game.add a' b').le (Game.add a b) ∧ (b.le b') → (a'.le a) := by
+  sorry
+
+
+theorem Game.add_equal (a b a' b' : Game) :
+  (a.eq a') ∧ (b.eq b') → (Game.add a b).eq (Game.add a' b') := by
+  sorry
+
+theorem Game.add_lt (a b a' b' : Game) :
+  (a.lt a') ∧ (b.le b') → (Game.add a b).lt (Game.add a' b') := by
+  sorry
+
+-- addition is commutative: check whether one needs a, b to be surreal
+theorem Game.add_comm (a b : Game) :
+  Game.add a b = Game.add b a := by
+  sorry
+
+theorem Game.add_assoc (a b c : Game) :
+  Game.add (Game.add a b) c = Game.add a (Game.add b c) := by
+  sorry
+
+def Game.neg : Game → Game
+  | g =>
+    let L := g.right.map (fun r => Game.neg r)
+    let R := g.left.map (fun l => Game.neg l)
+    Game.mk L R
+  termination_by g => g.birthday
+  decreasing_by
+    · sorry
+    · sorry
+
+theorem Game.add_neg (a : Game) :
+  (Game.add a (Game.neg a)).eq zero := by
+  sorry
+
+theorem Game.neg_add (a : Game) :
+  (Game.add (Game.neg a) a).eq zero := by
+  sorry
+
+theorem Game.neg_isSurreal (a : Surreal) :
+  IsSurreal (Game.neg a) := by
+  sorry
+
+theorem Game.add_isSurreal (a b : Surreal) :
+  IsSurreal (Game.add a b) := by
+  sorry
